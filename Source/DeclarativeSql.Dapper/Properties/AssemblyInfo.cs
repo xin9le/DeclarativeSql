feaf0825095239057e67be4843eab04ca27288ab
Takaaki Suzuki <anonymous-geek@live.jp>
2015-05-15 23:49:40 +0900

    v0.1.10 をリリース

--- Source/DeclarativeSql.Dapper/Properties/AssemblyInfo.cs
+++ Source/DeclarativeSql.Dapper/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.9.0")]
-[assembly: AssemblyFileVersion("0.1.9.0")]
+[assembly: AssemblyVersion("0.1.10.0")]
+[assembly: AssemblyFileVersion("0.1.10.0")]
