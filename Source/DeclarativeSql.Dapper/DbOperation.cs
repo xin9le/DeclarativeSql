2685065b13a2d9ebff4a30864b175f8d0b70a760
Takaaki Suzuki <anonymous-geek@live.jp>
2016-01-19 01:08:57 +0900

    refs #21 Add SQL Server Compact support.

--- Source/DeclarativeSql.Dapper/DbOperation.cs
+++ Source/DeclarativeSql.Dapper/DbOperation.cs
@@ -594,7 +594,7 @@ namespace DeclarativeSql.Dapper
 
 
     /// <summary>
-    /// SqlServerデータベースに対する操作を提供します。
+    /// Sql Serverデータベースに対する操作を提供します。
     /// </summary>
     internal class SqlServerOperation : DbOperation
     {
@@ -704,6 +704,39 @@ namespace DeclarativeSql.Dapper
             =>
 $@"{PrimitiveSql.CreateInsert<T>(this.DbKind)};
 select cast(scope_identity() as bigint) as Id;";
+        #endregion
+    }
+
+
+
+    /// <summary>
+    /// SQL Server Compactデータベースに対する操作を提供します。
+    /// </summary>
+    internal class SqlServerCeOperation : DbOperation
+    {
+        #region コンストラクタ
+        /// <summary>
+        /// インスタンスを生成します。
+        /// </summary>
+        /// <param name="connection">データベース接続</param>
+        /// <param name="transaction">トランザクション</param>
+        /// <param name="timeout">タイムアウト時間</param>
+        protected SqlServerCeOperation(IDbConnection connection, IDbTransaction transaction, int? timeout)
+            : base(connection, transaction, timeout)
+        {}
+        #endregion
+
+
+        #region InsertAndGet
+        /// <summary>
+        /// レコードを挿入し、そのレコードに自動採番されたIDを取得するSQLを生成します。
+        /// </summary>
+        /// <typeparam name="T">テーブルにマッピングされた型</typeparam>
+        /// <returns>SQL文</returns>
+        protected override string CreateInsertAndGetSql<T>()
+            =>
+$@"{PrimitiveSql.CreateInsert<T>(this.DbKind)};
+select @@IDENTITY as Id;";
         #endregion
     }
 
