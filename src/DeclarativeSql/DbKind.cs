4a23218adb1b9f37af9cb06788a2cfb1dda0a481
Takaaki Suzuki <xin9le@live.jp>
2016-11-28 01:08:45 +0900

    Add DbKind

---
+++ src/DeclarativeSql/DbKind.cs
@@ -0,0 +1,42 @@
+﻿namespace DeclarativeSql
+{
+    /// <summary>
+    /// Represents kind of database.
+    /// </summary>
+    public enum DbKind
+    {
+        /// <summary>
+        /// SQL Server
+        /// </summary>
+        SqlServer = 0,
+
+        /*
+        /// <summary>
+        /// SQL Server Compact
+        /// </summary>
+        SqlServerCe,
+
+        /// <summary>
+        /// Oracle
+        /// </summary>
+        Oracle,
+        */
+
+        /// <summary>
+        /// MySQL / Amazon Aurora / MariaDB
+        /// </summary>
+        MySql,
+
+        /*
+        /// <summary>
+        /// PostgreSQL
+        /// </summary>
+        PostgreSql,
+
+        /// <summary>
+        /// SQLite
+        /// </summary>
+        SQLite,
+        */
+    }
+}
