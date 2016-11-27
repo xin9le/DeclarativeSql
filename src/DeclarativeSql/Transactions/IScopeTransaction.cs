fe8c32b242ce548c873d73370c91948d4cfc2f8e
Takaaki Suzuki <xin9le@live.jp>
2016-11-28 01:16:53 +0900

    Add scope based transaction

---
+++ src/DeclarativeSql/Transactions/IScopeTransaction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+
+namespace DeclarativeSql.Transactions
+{
+    /// <summary>
+    /// Provides scope manageable transactions interface.
+    /// </summary>
+    public interface IScopeTransaction : IDisposable
+    {
+        #region Methods
+        /// <summary>
+        /// Mark whether the process has completed successfully.
+        /// </summary>
+        void Complete();
+        #endregion
+    }
+}
