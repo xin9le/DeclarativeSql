3c9d8e01aef55306ace055f3aeb8d9d0a3613d35
Takaaki Suzuki <xin9le@live.jp>
2016-11-28 01:09:37 +0900

    Add annotations

---
+++ src/DeclarativeSql/Annotations/AutoIncrementAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+
+namespace DeclarativeSql.Annotations
+{
+    /// <summary>
+    /// Represents an attribute representing automatic number assignment.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class AutoIncrementAttribute : Attribute
+    {
+        #region Constructors
+        /// <summary>
+        /// Creates instance.
+        /// </summary>
+        public AutoIncrementAttribute()
+        {}
+        #endregion
+    }
+}
