939ce46c2b1d56ccbe273e509cd77f4d3b6db7e5
Takaaki Suzuki <xin9le@live.jp>
2017-01-04 03:59:27 +0900

    Remove comment

--- src/DeclarativeSql/SqlGenerator.cs
+++ src/DeclarativeSql/SqlGenerator.cs
@@ -268,8 +268,6 @@ namespace DeclarativeSql
             var root = PredicateParser.Parse(predicate);
 
             //--- バインド変数の個数の桁を算出
-            //--- 10 で割った回数で算出するのが最も早いはず
-            //--- http://smdn.jp/programming/netfx/tips/get_number_of_digits/
             var parameterCount  = root.DescendantsAndSelf().Count(x =>
                                 {
                                     return  x.Operator != PredicateOperator.AndAlso
