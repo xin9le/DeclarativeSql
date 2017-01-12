0a2822ffbdde72d835df43e584fddb59010b259c
Takaaki Suzuki <xin9le@live.jp>
2017-01-13 02:13:19 +0900

    Add null check.

--- src/DeclarativeSql/ClauseBuilder.cs
+++ src/DeclarativeSql/ClauseBuilder.cs
@@ -18,7 +18,11 @@ namespace DeclarativeSql
         /// <param name="provider">Database provider</param>
         /// <returns>Count clause</returns>
         public static ICountClause<T> Count<T>(this DbProvider provider)
-            => new CountClause<T>(provider, null);
+        {
+            if (provider == null)
+                throw new ArgumentNullException(nameof(provider));
+            return new CountClause<T>(provider, null);
+        }   
 
 
         /// <summary>
@@ -29,7 +33,11 @@ namespace DeclarativeSql
         /// <param name="properties">Property expressions mapped columns. If null, targets all columns.</param>
         /// <returns>Select clause</returns>
         public static ISelectClause<T> Select<T>(this DbProvider provider, Expression<Func<T, object>> properties = null)
-            => new SelectClause<T>(provider, null, properties);
+        {
+            if (provider == null)
+                throw new ArgumentNullException(nameof(provider));
+            return new SelectClause<T>(provider, null, properties);
+        }
 
 
         /// <summary>
@@ -41,7 +49,11 @@ namespace DeclarativeSql
         /// <param name="setIdentity">Whether set identity</param>
         /// <returns>Insert clause</returns>
         public static IInsertClause<T> Insert<T>(this DbProvider provider, bool useSequence = true, bool setIdentity = false)
-            => new InsertClause<T>(provider, null, useSequence, setIdentity);
+        {
+            if (provider == null)
+                throw new ArgumentNullException(nameof(provider));
+            return new InsertClause<T>(provider, null, useSequence, setIdentity);
+        }
 
 
         /// <summary>
@@ -53,7 +65,11 @@ namespace DeclarativeSql
         /// <param name="setIdentity">Whether set identity</param>
         /// <returns>Update clause</returns>
         public static IUpdateClause<T> Update<T>(this DbProvider provider, Expression<Func<T, object>> properties = null, bool setIdentity = false)
-            => new UpdateClause<T>(provider, null, properties, setIdentity);
+        {
+            if (provider == null)
+                throw new ArgumentNullException(nameof(provider));
+            return new UpdateClause<T>(provider, null, properties, setIdentity);
+        }
 
 
         /// <summary>
@@ -63,7 +79,11 @@ namespace DeclarativeSql
         /// <param name="provider">Database provider</param>
         /// <returns>Delete clause</returns>
         public static IDeleteClause<T> Delete<T>(this DbProvider provider)
-            => new DeleteClause<T>(provider, null);
+        {
+            if (provider == null)
+                throw new ArgumentNullException(nameof(provider));
+            return new DeleteClause<T>(provider, null);
+        }
 
 
         /// <summary>
@@ -73,7 +93,11 @@ namespace DeclarativeSql
         /// <param name="provider">Database provider</param>
         /// <returns>Truncate clause</returns>
         public static ITruncateClause<T> Truncate<T>(this DbProvider provider)
-            => new TruncateClause<T>(provider, null);
+        {
+            if (provider == null)
+                throw new ArgumentNullException(nameof(provider));
+            return new TruncateClause<T>(provider, null);
+        }
 
 
         /// <summary>
@@ -84,7 +108,11 @@ namespace DeclarativeSql
         /// <param name="predicate">Predicative expression</param>
         /// <returns>Where clause</returns>
         public static IWhereClause<T> Where<T>(this DbProvider provider, Expression<Func<T, bool>> predicate)
-            => new WhereClause<T>(provider, null, predicate);
+        {
+            if (provider == null)  throw new ArgumentNullException(nameof(provider));
+            if (predicate == null) throw new ArgumentNullException(nameof(predicate));
+            return new WhereClause<T>(provider, null, predicate);
+        }
         #endregion
 
 
@@ -97,7 +125,11 @@ namespace DeclarativeSql
         /// <param name="predicate">Predicative expression</param>
         /// <returns>Where clause</returns>
         public static IWhereClause<T> Where<T>(this ICountClause<T> clause, Expression<Func<T, bool>> predicate)
-            => new WhereClause<T>(clause.DbProvider, clause, predicate);
+        {
+            if (clause == null)    throw new ArgumentNullException(nameof(clause));
+            if (predicate == null) throw new ArgumentNullException(nameof(predicate));
+            return new WhereClause<T>(clause.DbProvider, clause, predicate);
+        }
 
 
         /// <summary>
@@ -108,7 +140,11 @@ namespace DeclarativeSql
         /// <param name="predicate">Predicative expression</param>
         /// <returns>Where clause</returns>
         public static IWhereClause<T> Where<T>(this ISelectClause<T> clause, Expression<Func<T, bool>> predicate)
-            => new WhereClause<T>(clause.DbProvider, clause, predicate);
+        {
+            if (clause == null)    throw new ArgumentNullException(nameof(clause));
+            if (predicate == null) throw new ArgumentNullException(nameof(predicate));
+            return new WhereClause<T>(clause.DbProvider, clause, predicate);
+        }
 
 
         /// <summary>
@@ -119,7 +155,11 @@ namespace DeclarativeSql
         /// <param name="predicate">Predicative expression</param>
         /// <returns>Where clause</returns>
         public static IWhereClause<T> Where<T>(this IUpdateClause<T> clause, Expression<Func<T, bool>> predicate)
-            => new WhereClause<T>(clause.DbProvider, clause, predicate);
+        {
+            if (clause == null)    throw new ArgumentNullException(nameof(clause));
+            if (predicate == null) throw new ArgumentNullException(nameof(predicate));
+            return new WhereClause<T>(clause.DbProvider, clause, predicate);
+        }
 
 
         /// <summary>
@@ -130,7 +170,11 @@ namespace DeclarativeSql
         /// <param name="predicate">Predicative expression</param>
         /// <returns>Where clause</returns>
         public static IWhereClause<T> Where<T>(this IDeleteClause<T> clause, Expression<Func<T, bool>> predicate)
-            => new WhereClause<T>(clause.DbProvider, clause, predicate);
+        {
+            if (clause == null)    throw new ArgumentNullException(nameof(clause));
+            if (predicate == null) throw new ArgumentNullException(nameof(predicate));
+            return new WhereClause<T>(clause.DbProvider, clause, predicate);
+        }
         #endregion
     }
 }
