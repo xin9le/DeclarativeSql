624f1c31bf526937f8ecc725f5ae876c0da59bbc
Takaaki Suzuki <xin9le@live.jp>
2019-10-09 19:53:07 +0900

    Add mapping information

---
+++ src/DeclarativeSql/Mapping/ColumnInfo.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using DeclarativeSql.Annotations;
+
+
+
+namespace DeclarativeSql.Mapping
+{
+    /// <summary>
+    /// Provides column mapping information.
+    /// </summary>
+    public sealed class ColumnInfo
+    {
+        #region Properties
+        /// <summary>
+        /// Gets the name of member.
+        /// </summary>
+        public string MemberName { get; }
+
+
+        /// <summary>
+        /// Gets the type of member.
+        /// </summary>
+        public Type MemberType { get; }
+
+
+        /// <summary>
+        /// Gets the name of column.
+        /// </summary>
+        public string ColumnName { get; }
+
+
+        /// <summary>
+        /// Gets the type of column.
+        /// </summary>
+        public DbType? ColumnType { get; }
+
+
+        /// <summary>
+        /// Gets whether primary key.
+        /// </summary>
+        public bool IsPrimaryKey { get; }
+
+
+        /// <summary>
+        /// Gets whether auto increment numbering.
+        /// </summary>
+        public bool IsAutoIncrement { get; }
+
+
+        /// <summary>
+        /// Gets whether allowing null.
+        /// </summary>
+        public bool AllowNull { get; }
+
+
+        /// <summary>
+        /// Gets whether unique constraint.
+        /// </summary>
+        public bool IsUnique
+            => this.UniqueIndex.HasValue;
+
+
+        /// <summary>
+        /// Gets the index of unique constraint.
+        /// </summary>
+        public int? UniqueIndex { get; }
+        #endregion
+
+
+        #region Constructors
+        /// <summary>
+        /// Creates instance.
+        /// </summary>
+        /// <param name="database"></param>
+        /// <param name="property"></param>
+        internal ColumnInfo(DbKind database, PropertyInfo property)
+            : this(database, property, property.PropertyType)
+        {}
+
+
+        /// <summary>
+        /// Creates instance.
+        /// </summary>
+        /// <param name="database"></param>
+        /// <param name="field"></param>
+        internal ColumnInfo(DbKind database, FieldInfo field)
+            : this(database, field, field.FieldType)
+        {}
+
+
+        /// <summary>
+        /// Creates instance.
+        /// </summary>
+        /// <param name="database"></param>
+        /// <param name="member"></param>
+        private ColumnInfo(DbKind database, MemberInfo member, Type memberType)
+        {
+            var column = member.GetCustomAttributes<ColumnAttribute>(true).FirstOrDefault(x => x.Database == database);
+            var unique = member.GetCustomAttribute<UniqueAttribute>(true);
+
+            this.MemberName = member.Name;
+            this.MemberType = memberType;
+            this.ColumnName = column?.Name ?? member.Name;
+            this.ColumnType = column?.Type;
+            this.IsPrimaryKey = Attribute.IsDefined(member, typeof(PrimaryKeyAttribute));
+            this.IsAutoIncrement = Attribute.IsDefined(member, typeof(AutoIncrementAttribute));
+            this.AllowNull = Attribute.IsDefined(member, typeof(AllowNullAttribute));
+            this.UniqueIndex = this.IsPrimaryKey ? -1 : (int?)unique?.Index;
+        }
+        #endregion
+    }
+}
---
+++ src/DeclarativeSql/Mapping/TableInfo.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using DeclarativeSql.Annotations;
+
+
+
+namespace DeclarativeSql.Mapping
+{
+    /// <summary>
+    /// Provides table mapping information.
+    /// </summary>
+    public sealed class TableInfo
+    {
+        #region Properties
+        /// <summary>
+        /// Gets the kind of database.
+        /// </summary>
+        public DbKind Database { get; private set; }
+
+
+        /// <summary>
+        /// Gets the type that is mapped to the table.
+        /// </summary>
+        public Type Type { get; private set; }
+
+
+        /// <summary>
+        /// Gets the schema name.
+        /// </summary>
+        public string Schema { get; private set; }
+
+
+        /// <summary>
+        /// Gets the table name.
+        /// </summary>
+        public string Name { get; private set; }
+
+
+        /// <summary>
+        /// Gets the full table name.
+        /// </summary>
+        public string FullName { get; private set; }
+
+
+        /// <summary>
+        /// Gets the column mapping information.
+        /// </summary>
+        public ReadOnlyArray<ColumnInfo> Columns { get; private set; }
+        #endregion
+
+
+        #region Constructors
+        /// <summary>
+        /// Creates instance.
+        /// </summary>
+        private TableInfo()
+        {}
+        #endregion
+
+
+        #region Get
+        /// <summary>
+        /// Gets the table mapping information corresponding to the specified type.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="database"></param>
+        /// <returns></returns>
+        public static TableInfo Get<T>(DbKind database)
+            => Cache<T>.Instances.TryGetValue(database, out var value)
+            ? value
+            : throw new NotSupportedException();
+        #endregion
+
+
+        #region Internal Cache
+        /// <summary>
+        /// Provides <see cref="TableInfo"/> cache.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        private static class Cache<T>
+        {
+            /// <summary>
+            /// Gets the instances by <see cref="DbKind"/>.
+            /// </summary>
+            public static IReadOnlyDictionary<DbKind, TableInfo> Instances { get; }
+
+
+            /// <summary>
+            /// Static constructors
+            /// </summary>
+            static Cache()
+            {
+                var type = typeof(T);
+                var flags = BindingFlags.Instance | BindingFlags.Public;
+                var attributes = type.GetCustomAttributes<TableAttribute>(true).ToDictionary(x => x.Database);
+                var properties = type.GetProperties(flags);
+                var fields = type.GetFields(flags);
+                var dbs = Enum.GetValues(typeof(DbKind)) as DbKind[];
+                var result = new Dictionary<DbKind, TableInfo>(dbs.Length);
+                foreach (var db in dbs)
+                {
+                    attributes.TryGetValue(db, out var table);
+                    var provider = DbProvider.ByDatabase[db];
+                    var b = provider.KeywordBracket;
+                    var schema = table?.Schema ?? provider.DefaultSchema;
+                    var name = table?.Name ?? type.Name;
+                    result[db] = new TableInfo
+                    {
+                        Database = db,
+                        Type = type,
+                        Schema = schema,
+                        Name = name,
+                        FullName
+                            = string.IsNullOrEmpty(schema)
+                            ? $"{b.Begin}{name}{b.End}"
+                            : $"{b.Begin}{schema}{b.End}.{b.Begin}{name}{b.End}",
+                        Columns
+                            = properties.Select(x => new ColumnInfo(db, x))
+                            .Concat(fields.Select(x => new ColumnInfo(db, x)))
+                            .ToReadOnlyArray(),
+                    };
+                }
+                Instances = result;
+            }
+        }
+        #endregion
+    }
+}
