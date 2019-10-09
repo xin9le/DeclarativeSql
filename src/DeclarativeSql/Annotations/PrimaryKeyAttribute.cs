24db40d76f14c51c4be2770865a0d1d1f924b89c
Takaaki Suzuki <xin9le@live.jp>
2019-10-09 19:10:19 +0900

    Add attributes

---
+++ src/DeclarativeSql/Annotations/PrimaryKeyAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+
+namespace DeclarativeSql.Annotations
+{
+    /// <summary>
+    /// Provides an attribute that represents a primary key.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+    public sealed class PrimaryKeyAttribute : Attribute
+    {
+        #region Constructors
+        /// <summary>
+        /// Creates instance.
+        /// </summary>
+        public PrimaryKeyAttribute()
+        {}
+        #endregion
+    }
+}
---
+++ src/DeclarativeSql/Annotations/UniqueAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+
+namespace DeclarativeSql.Annotations
+{
+    /// <summary>
+    /// Provides an attribute that represents a unique constraint.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+    public sealed class UniqueAttribute : Attribute
+    {
+        #region Properties
+        /// <summary>
+        /// Gets the index.
+        /// </summary>
+        public ushort Index { get; }
+        #endregion
+
+
+        #region Constructors
+        /// <summary>
+        /// Creates instance.
+        /// </summary>
+        public UniqueAttribute(ushort index)
+            => this.Index = index;
+        #endregion
+    }
+}
