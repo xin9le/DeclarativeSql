b0679f3c2af35d567a7419e62c3ab7785934be4a
Takaaki Suzuki <xin9le@live.jp>
2019-10-10 12:59:00 +0900

    Add query

---
+++ src/DeclarativeSql/Sql/Query.cs
@@ -0,0 +1,35 @@
+﻿namespace DeclarativeSql.Sql
+{
+    /// <summary>
+    /// Represents a query statement and parameter pair.
+    /// </summary>
+    public sealed class Query
+    {
+        #region Properties
+        /// <summary>
+        /// Gets SQL statement.
+        /// </summary>
+        public string Statement { get; }
+
+
+        /// <summary>
+        /// Gets bind parameter.
+        /// </summary>
+        public BindParameter BindParameter { get; }
+        #endregion
+
+
+        #region Constructors
+        /// <summary>
+        /// Creates instance.
+        /// </summary>
+        /// <param name="statement"></param>
+        /// <param name="bindParameter"></param>
+        public Query(string statement, BindParameter bindParameter)
+        {
+            this.Statement = statement;
+            this.BindParameter = bindParameter;
+        }
+        #endregion
+    }
+}
