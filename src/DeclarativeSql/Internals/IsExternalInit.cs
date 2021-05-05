190cc6c1a3ab206e8fb6b2c0b8e1a122d6049d60
Takaaki Suzuki <xin9le@live.jp>
2021-05-05 16:37:15 +0900

    Enable init keyword

---
+++ src/DeclarativeSql/Internals/IsExternalInit.cs
@@ -0,0 +1,10 @@
+﻿#if !NET5_0_OR_GREATER
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Runtime.CompilerServices
+{
+    internal sealed class IsExternalInit
+    { }
+}
+#endif
